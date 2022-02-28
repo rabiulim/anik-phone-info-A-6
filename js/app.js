7a9e85de88cc90199776d62a2e0503c79813cba9
rabiulim <rabiulim2022@gmail.com>
2022-03-01 00:22:29 +0600

    display details done

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const getPhoneInfo = () => {
 }
 
 const displayPhone = (phones) => {
+    console.log(phones[1])
     for (const phone of phones) {
         console.log(phone)
         const div = document.createElement('div')
@@ -21,10 +22,26 @@ const displayPhone = (phones) => {
        <div class="card-body">
          <h5 class="card-title">Card title</h5>
          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <button href="#" class="btn btn-primary">Details</button>
+         <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
        </div>
      </div>
        `
         main.appendChild(div)
     }
+}
+
+const phoneDetails = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=`)
+        .then(res => res.json())
+        .then(data => {
+            const allPhone = data.data
+            const singlePhone = allPhone.find(phone => phone.slug === id)
+            console.log(singlePhone)
+            const div = document.createElement('div')
+            main.innerHTML = '';
+            div.innerHTML = `
+            <img  src="${singlePhone.image}" class="card-img-top" alt="...">
+            `
+            main.appendChild(div)
+        })
 }
